1338f4359a3b0153aef31b37ff1b1059da94216c
k9845 <khatririshi2430@gmail.com>
2024-09-07 13:39:11 +0545

    Add login page

---
+++ src/components/Login.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+interface LoginFormData {
+  usernmae: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const [formData, setFormData] = useState<LoginFormData>({ usernmae: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setErrorMessage('');
+
+    try {
+      const response = await axios.post<{ token: string }>('http://localhost:8000/login/', formData);
+      const { token } = response.data;
+
+      localStorage.setItem('token', token);
+
+      navigate('/');
+    } catch (error) {
+      setErrorMessage('Invalid email or password');
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-8">
+        Supply Chain Management
+      </h1>
+
+      <div className="bg-white shadow-md rounded-lg px-8 py-6 w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center mb-4">Login</h2>
+
+        {errorMessage && (
+          <p className="text-red-500 text-center mb-4">{errorMessage}</p>
+        )}
+
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700">Username:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.usernmae}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              required
+            />
+          </div>
+
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
