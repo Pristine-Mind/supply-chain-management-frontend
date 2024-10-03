20fc5f9701d4f8b9669b7ba4605825e0c3a83d12
k9845 <khatririshi2430@gmail.com>
2024-10-03 18:56:08 +0545

    Add back button on required page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import AddProducer from './components/AddProducer';
@@ -10,34 +10,48 @@ import Stocks from './components/Stocks';
 import ProductInstanceView from './components/ProductInstanceView';  
 import StatsDashboard from './components/StatsDashboard';
 // import Marketplace from './components/Marketplace';
+import BackButton from './components/BackButton';
 
 const App: React.FC = () => {
-  // const isAuthenticated = !!localStorage.getItem('token');
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/home"
-          element={<Home /> }
-        />
-        <Route path="/producers" element={<AddProducer />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/customers" element={<CustomerList />} />
-        <Route path="/orders" element={<OrderList />} />
-        <Route path="/sales" element={<SaleList />} />
-        <Route path="/stocks" element={<Stocks />} />
-        <Route path="/stats" element={<StatsDashboard />} />
-        <Route path="/marketplace/:productId" element={<ProductInstanceView />} />
-        
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <div className="p-4">
+        <ConditionalBackButton />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/producers" element={<AddProducer />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/customers" element={<CustomerList />} />
+          <Route path="/orders" element={<OrderList />} />
+          <Route path="/sales" element={<SaleList />} />
+          <Route path="/stocks" element={<Stocks />} />
+          <Route path="/stats" element={<StatsDashboard />} />
+          <Route path="/marketplace/:productId" element={<ProductInstanceView />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
 
+const ConditionalBackButton: React.FC = () => {
+  const location = useLocation();
+  const routesWithBackButton = [
+    '/producers',
+    '/products',
+    '/customers',
+    '/orders',
+    '/sales',
+    '/stocks',
+    '/stats',
+    '/marketplace'
+  ];
 
+  const shouldShowBackButton = routesWithBackButton.some(route => location.pathname.startsWith(route));
+
+  return shouldShowBackButton ? <BackButton /> : null;
+};
 
 export default App;
